249bd2a66664359a7c873d22bc4c62d17052b49b
Ayrton-Neacail <ayrtona90@gmail.com>
2016-03-16 12:28:26 +0200

    added the move method

--- Tut4-TicTacToe/TicTacToe.cpp
+++ Tut4-TicTacToe/TicTacToe.cpp
@@ -22,3 +22,19 @@ using namespace std;
 		 for (int j = 1; j <= 3; j++)
 			 grid[i][j] = '-';
  };
+ bool TICTAC::move(int row, int col, int player)
+ {
+	 bool flag = 0;
+	 if (row <= 3 && col <= 3 && row > 0 && col > 0)
+	 {
+		 if (grid[row][ccol] == '-')//checks if the space available
+		 {
+			 flag = 1;
+			 if (player == 1)
+				 grid[row][col] = 'x';
+			 else
+				 grid[row][col] = 'o';
+		 };
+	 };
+	 return flag;
+ };
--- Tut4-TicTacToe/mainTTT.cpp
+++ Tut4-TicTacToe/mainTTT.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <TicTac.h>
+#include "TicTac.h"
 
 
 using namespace std;
