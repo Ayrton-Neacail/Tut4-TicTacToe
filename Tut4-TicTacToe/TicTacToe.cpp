3f769c9939e362c069793290bae671766b125c3d
Ayrton-Neacail <ayrtona90@gmail.com>
2016-03-16 12:25:35 +0200

    coded reset function, added default constructor and destructor

--- Tut4-TicTacToe/TicTacToe.cpp
+++ Tut4-TicTacToe/TicTacToe.cpp
@@ -1,12 +1,24 @@
-#include <TicTac.h>
+#include "TicTac.h"
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-int main(){
+ TICTAC::TICTAC()         //initializes grid for new play
+{
+	for (int i = 1; i <= 3; i++)
+		for (int j = 1; j <= 3; j++)
+			grid[i][j] = '-';
+};
 
+ TICTAC::~TICTAC()
+ {
+	 //DEFAULT destructor
+ };
 
-
-
-}
+ void TICTAC::reset() //resets the game grid
+ {
+	 for (int i = 1; i <= 3; i++)
+		 for (int j = 1; j <= 3; j++)
+			 grid[i][j] = '-';
+ };
